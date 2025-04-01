88107229010ded3b07643332612f266d49aa3472
thomthomtt <thomazgt39@gmail.com>
2025-04-01 16:19:33 -0300

    media das notas

--- exercicio06.py
+++ exercicio06.py
@@ -3,6 +3,11 @@ nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 media = (nota1+nota2+nota3)/3
 if media>=7:
-    print(f"Você está Aprovado{media}")
+    print(f"Você está Aprovado {media}")
 else:
-    print(f"Você está Reprovado{media}")
+    if media < 4:
+        print(f"Você está reprovado {media}")
+    else:
+        print(f"Você está em recuperação {media}")
+
+
