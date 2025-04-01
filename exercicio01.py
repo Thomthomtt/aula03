2cde396eb4e7cbebfdb817735dc13ee43cf34e6f
thomthomtt <thomazgt39@gmail.com>
2025-04-01 13:57:57 -0300

    programa pra ler nome, idade e salario

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome completo: ")
+idade = int(input("Digite sua idade: "))
+salario = float(input("Digite seu salario: "))
+print(f"Seu nome é: {nome}, sua idade é {idade} e seu salario é {salario}!")
