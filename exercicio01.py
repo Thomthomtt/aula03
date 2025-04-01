7a522cebe07b7bc3223056d498d4fe027269a174
thomthomtt <thomazgt39@gmail.com>
2025-04-01 14:19:48 -0300

    Comando para pegar o aumento do salario da pessoa

--- exercicio01.py
+++ exercicio01.py
@@ -1,4 +1,7 @@
 nome = input("Digite seu nome completo: ")
 idade = int(input("Digite sua idade: "))
 salario = float(input("Digite seu salario: "))
-print(f"Seu nome é: {nome}, sua idade é {idade} e seu salario é {salario}!")
+aumento = float(input("Qual a porcentagem de aumento?"))
+calc_aumento = (salario*aumento)/100
+soma_salario = calc_aumento+salario
+print(f"Seu nome é: {nome}, sua idade é {idade} e seu salario é {salario:.2f}! Parabêns, você recebeu um aumento de: {calc_aumento}. O seu salario atual é: {soma_salario}!")
