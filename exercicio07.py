e6daae8ebd5581e376ecb7f6fe79ed48f660ab8b
thomthomtt <thomazgt39@gmail.com>
2025-04-01 16:29:01 -0300

    contagem de gols de time para ver qual foi o vencedor

---
+++ exercicio07.py
@@ -0,0 +1,8 @@
+time1 = input("Qual o nome do primeiro time? ")
+gols1 = int(input("Quantos gols foram marcados pelo primeito time? "))
+time2 = input("Qual o nome do segundo time? ")
+gols2 = int(input("Quantos gols foram marcados pelo segundo time? "))
+if gols1>gols2:
+    print(f"O time {time1} foi o vencedor com {gols1} gols")
+else:
+    print(f"O time {time2} foi o vencedor com {gols2} gols")
