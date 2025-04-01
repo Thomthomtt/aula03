aa4e4cd12d812021719f4437bd8699128726701d
thomthomtt <thomazgt39@gmail.com>
2025-04-01 14:48:23 -0300

    4 operacoes com 2 numeros

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+soma = num1+num2
+sub = num1-num2
+mult = num1*num2
+divs = num1/num2
+print(f"A soma de {num1} + {num2} é: {soma}.")
+print(f"A subtração de {num1} - {num2} é: {sub}.")
+print(f"A multiplicação de {num1} * {num2} é: {mult}.")
+print(f"A divisão de {num1} / {num2} é: {divs}.")
+
