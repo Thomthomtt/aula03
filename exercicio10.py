a259f16e4e6599bbab8122caf952e590ead8a598
thomthomtt <thomazgt39@gmail.com>
2025-04-08 15:29:14 -0300

    Codigo exercicio 10

---
+++ exercicio10.py
@@ -0,0 +1,57 @@
+num = int(input("Digite um numero de 1 a 12: "))
+if num > 12 or num < 1:
+    print(f"O valor não corresponde ao solicitado")
+if num==1:
+    print(f"Janeiro")
+elif num==2:
+    print(f"Fevereiro")
+elif num==3:
+    print(f"Março")
+elif num==4:
+    print(f"Abril")
+elif num==5:
+    print(f"Maio")
+elif num==6:
+    print(f"Junho")
+elif num==7:
+    print(f"Julho")
+elif num==8:
+    print(f"Agosto")
+elif num==9:
+    print(f"Setembro")
+elif num==10:
+    print(f"Outubro")
+elif num==11:
+    print(f"Novembro")
+elif num==12:
+    print(f"Dezembro")
+
+"""  CORREÇÃO  
+num = int(input("Digite um numero de 1 a 12: "))
+    if num >=1 or num <=12:
+        if num == 1:
+            print(f"Janeiro")
+        elif num == 2:
+            print(f"Fevereiro")
+        elif num == 3:
+            print(f"Março")
+        elif num == 4:
+            print(f"Abril")
+        elif num == 5:
+            print(f"Maio")
+        elif num == 6:
+            print(f"Junho")
+        elif num == 7:
+            print(f"Julho")
+        elif num == 8:
+            print(f"Agosto")
+        elif num == 9:
+            print(f"Setembro")
+        elif num == 10:
+            print(f"Outubro")
+        elif num == 11:
+            print(f"Novembro")
+        else num == 12:
+            print(f"Dezembro")
+    else:
+print(f"O valor não corresponde ao solicitado")"""
